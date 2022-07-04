28c538359d7e2e44be463eec6f0721293a6b11c7
Anatoliy Vlasenko <anatoliy.vlasko@mail.ru>
2022-07-04 16:56:31 +0600

    t_3 modiffied

--- Lesson_3/t_2/Program.cs
+++ Lesson_3/t_2/Program.cs
@@ -5,7 +5,7 @@
 
 double XY (double x1, double x2, double y1, double y2)
 {
-    return Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y2-y1,2));
+    return Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2));
 }
 
 XY(3,6,2,1);
