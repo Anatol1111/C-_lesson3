692541419f78a2c218f4c365d7373672805a708d
Anatoliy Vlasenko <anatoliy.vlasko@mail.ru>
2022-07-04 18:36:57 +0600

    Homework3_1 modiffied

---
+++ Lesson_3/Homework3_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1: Напишите программу, которая принимает на
+// вход пятизначное число и проверяет, является ли оно
+// палиндромом.
+
+void Palindrome(int n)
+{
+int i = 0;
+string num = n.ToString();
+int size = num.Length - 1;
+string flag = "yes";
+
+while(i < size / 2)
+{
+ if (num[i] != num[size - i])
+ {
+    flag = "no";
+    break;
+ }
+ i++;
+}
+Console.WriteLine($"{n}--> {flag}");
+}
+
+Palindrome(23432);
+Palindrome(12345);
